bd11904ba4bf017ac92ef7d3f8a02c2975c103c4
abdur4430 <114722926+abdur4430@users.noreply.github.com>
2025-06-30 13:27:29 +0500

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:hello@abdus4430.com")
                       }
                     >
                       Resume
@@ -85,7 +85,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:hello@abdus4430.com")}
                   >
                     Contact
                   </Button>
